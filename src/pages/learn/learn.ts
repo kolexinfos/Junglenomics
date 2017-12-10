30c705cd82fbcfefa472ee4874b8c05603348624
Kehinde Kolade <kolexinfos@gmail.com>
2017-12-10 08:48:53 +0100

    Updated Changes to File Download

--- src/pages/learn/learn.ts
+++ src/pages/learn/learn.ts
@@ -35,7 +35,7 @@ export class LearnPage {
     const preview = 'http://www.preptitude.com.ng/preview.pdf';
     const playbook = 'http://www.preptitude.com.ng/playbook.pdf';
     
-    this.fileTransfer.download(proposal, this.file.dataDirectory +
+    this.fileTransfer.download(proposal, this.file.externalDataDirectory +
       'proposal.pdf').then((entry) => {
         console.log('download complete: ' + entry.toURL());
       }, (error) => {
@@ -48,7 +48,7 @@ export class LearnPage {
         console.log(error);
       });
 
-      this.fileTransfer.download(preview, this.file.dataDirectory +
+      this.fileTransfer.download(preview, this.file.externalDataDirectory +
       'preview.pdf').then((entry) => {
         console.log('download complete: ' + entry.toURL());
 
@@ -64,7 +64,7 @@ export class LearnPage {
         console.log(error);
       });
 
-      this.fileTransfer.download(playbook, this.file.dataDirectory +
+      this.fileTransfer.download(playbook, this.file.externalDataDirectory +
       'playbook.pdf').then((entry) => {
         console.log('download complete: ' + entry.toURL());
 
